abe971ab7d33554e63a8c7f4899d5a2a5a55f4f1
Vitchakorn <112546047+Vitchakorn@users.noreply.github.com>
2023-05-29 11:36:36 +0700

    path

--- app.py
+++ app.py
@@ -11,6 +11,19 @@ graph = [[0, 2, 3, 4], [10, 0, 4, 7],
          [6, 1, 0, 8], [9, 12, 11, 0]]
 
 
+def client(address):
+    while True:
+        try:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((address, PORT))
+            message = client_socket.recv(1024).decode()
+            print(message)
+            client_socket.close()
+            break
+        except Exception as e:
+            continue
+
+
 def tsp(graph):
     n = len(graph)
     dp = [[float('inf')] * n for _ in range(1 << n)]
@@ -26,34 +39,35 @@ def tsp(graph):
                 dp[mask][current] = min(dp[mask][current], dp[mask ^ (1 << current)][prev] + graph[prev][current])
 
     min_cost = float('inf')
+    last_city = -1
     for i in range(1, n):
         cost = dp[(1 << n) - 1][i] + graph[i][0]
         if cost < min_cost:
             min_cost = cost
+            last_city = i
             
-    return min_cost
+    path = [0]
+    mask = (1 << n) - 1
+    while last_city != 0:
+        path.append(last_city)
+        mask ^= (1 << last_city)
+        for prev in range(n):
+            if mask & (1 << prev) == 0:
+                continue
+            if dp[mask][last_city] == dp[mask ^ (1 << last_city)][prev] + graph[prev][last_city]:
+                last_city = prev
+                break
+    path.append(0)
+
+    return path, min_cost
 
 
-def client(address):
-    while True:
-        try:
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client_socket.connect((address, PORT))
-            message = client_socket.recv(1024).decode()
-            print(message)
-            client_socket.close()
-            break
-        except Exception as e:
-            continue
-
-
-def handle_client(client_socket, client_address):
-    global total_cost
+def sending(server, address):
     node_host = IP.index(host_address)
-    node_client = IP.index(client_address[0])
+    node_client = IP.index(address[0])
     cost = graph[node_host][node_client]
-    message = str(node_client) + str(node_host) + " cost = {}".format(cost) 
-    client_socket.send(message.encode())
+    message = str(node_client) + str(node_host) + " cost = {} ".format(cost) + str(address[0]) + str(host_address)
+    server.send(message.encode())
 
 
 def server(address):
@@ -62,12 +76,13 @@ def server(address):
     server_socket.listen()
     clients = 0
     while True:
-        client_socket, client_address = server_socket.accept()
+        server , address = server_socket.accept()
         time.sleep(1)
         client_thread = threading.Thread(
-            target=handle_client, args=(client_socket, client_address))
+            target=sending, args=(server, address))
         client_thread.start()
         clients += 1
+
         if (clients == len(IP) - 1):
             break
     server_socket.close()
@@ -75,19 +90,19 @@ def server(address):
 
 def main():
     global index
+    print(index)
     if host_address == IP[index]:
         server(IP[index])
     else:
         client(IP[index])
-    
     index += 1
 
     if (index != len(IP)):
         main()
     else :
-        min_cost = tsp(graph)
+        path, min_cost = tsp(graph)
+        print("path:", path)
         print("Minimum Cost:", min_cost)
 
 main()
-print(index)
     
