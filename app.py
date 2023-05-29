7dff3046e1c9007d82287b3fd43039b367389490
Vitchakorn <112546047+Vitchakorn@users.noreply.github.com>
2023-05-29 10:35:16 +0700

    tsp

--- app.py
+++ app.py
@@ -7,8 +7,32 @@ IP = ['172.28.5.1', '172.28.5.2', '172.28.5.3', '172.28.5.4']
 host = socket.gethostname()
 host_address = socket.gethostbyname(host)
 index = 0 
-graph = [[0, 10, 15, 20], [10, 0, 35, 25],
-        [15, 35, 0, 30], [20, 25, 30, 0]]
+graph = [[0, 2, 3, 4], [10, 0, 4, 7], 
+         [6, 1, 0, 8], [9, 12, 11, 0]]
+
+
+def tsp(graph):
+    n = len(graph)
+    dp = [[float('inf')] * n for _ in range(1 << n)]
+    dp[1][0] = 0
+
+    for mask in range(1, 1 << n):
+        for current in range(n):
+            if mask & (1 << current) == 0:
+                continue
+            for prev in range(n):
+                if mask & (1 << prev) == 0:
+                    continue
+                dp[mask][current] = min(dp[mask][current], dp[mask ^ (1 << current)][prev] + graph[prev][current])
+
+    min_cost = float('inf')
+    for i in range(1, n):
+        cost = dp[(1 << n) - 1][i] + graph[i][0]
+        if cost < min_cost:
+            min_cost = cost
+            
+    return min_cost
+
 
 def client(address):
     while True:
@@ -24,10 +48,11 @@ def client(address):
 
 
 def handle_client(client_socket, client_address):
+    global total_cost
     node_host = IP.index(host_address)
     node_client = IP.index(client_address[0])
     cost = graph[node_host][node_client]
-    message = str(node_host) + str(node_client) + " and cost is {}".format(cost) 
+    message = str(node_client) + str(node_host) + " cost = {}".format(cost) 
     client_socket.send(message.encode())
 
 
@@ -59,6 +84,10 @@ def main():
 
     if (index != len(IP)):
         main()
+    else :
+        min_cost = tsp(graph)
+        print("Minimum Cost:", min_cost)
 
 main()
+print(index)
     
